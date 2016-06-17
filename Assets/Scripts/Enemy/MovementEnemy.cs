f325efdf0e7c29fc49ef0a7d05ee6528139fa02d
Ewout Bos <ewout_speed@hotmail.com>
2016-06-17 20:23:01 +0200

    Apperently My last push was not compleet so here ya go

--- Assets/Scripts/Enemy/MovementEnemy.cs
+++ Assets/Scripts/Enemy/MovementEnemy.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class MovementEnemy : MonoBehaviour {
 
+    private Rigidbody2D rBody2D;
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        rBody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        rBody2D.velocity = Vector2.zero;
 	}
 }
